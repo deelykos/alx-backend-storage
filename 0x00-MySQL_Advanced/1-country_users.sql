0ef243e17cc9b83a013df28dcbd9d996919bd015
deelykos <fabianohaz@gmail.com>
2024-05-01 16:12:33 +0000

    Creating a Table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,11 @@
--- Creating a table users
+-- Creating a Table users
 
--- Creating a table users with an additional attribute country.
-ALTER TABLE users
-ADD column country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL;
+-- Creating a Table called users with id, email, name, country.
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
 
